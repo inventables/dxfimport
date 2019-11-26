ad63df505c81638effdd90cc5771a3b95a4a47be
Kacper Marcisz <kmarcisz@gmail.com>
2019-11-26 10:22:54 -0600

    Check in the new version of the app create by Jelle

--- lambda/app.js
+++ lambda/app.js
@@ -1,15 +1,41 @@
-/**
- * exports.handler called by lambda
- *
- * This is a normal node package - if you'd like to add packages you can just
- *
- * npm install -g <package-name>
- */
-var dxf = require('dxf'),
-    request = require('request');
+'use strict';
 
-exports.handler = function(event, context) {
-  request({url: event['DXF File'], encoding: null}, function(error, response, body) {
-    context.done(null, {svg: dxf.toSVG(dxf.parseString(body.toString()))});
+var dxf = require('dxf'),
+  request = require('request');
+
+exports.handler = function (event, context) {
+  request({ url: event.url, encoding: null }, function (error, response, body) {
+    var dxfContents = body.toString();
+    var units = determineUnits(dxfContents);
+    var svgSTR = dxf.toSVG(dxf.parseString(dxfContents));
+    svgSTR = svgSTR.replace("INSUNITS", units);
+    svgSTR = svgSTR.replace("INSUNITS", units);
+    context.done(null, { svg: svgSTR });
   });
+};
+
+function determineUnits(dxfContents) {
+  var unitSVG = "INSUNITS";
+  if (dxfContents.includes("$INSUNITS")) {
+    var splitOnUnits = dxfContents.split("$INSUNITS");
+    var splitOnNine = splitOnUnits[1].split("9");
+    var splitOnSeventy = splitOnNine[0].split("70");
+    var units = splitOnSeventy[1].replace(/(\r\n\t|\n|\r\t)/gm, "");
+    var unit = parseInt(units);
+
+    switch (unit) {
+      case 1:
+        unitSVG = "in"
+        break;
+      case 4:
+        unitSVG = "mm"
+        break;
+      case 5:
+        unitSVG = "cm"
+        break;
+      default:
+        unitSVG = "INSUNITS"
+    }
+  }
+  return unitSVG;
 }
