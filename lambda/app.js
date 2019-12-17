b980d940decbd4ac997118dae28b93c2e19811da
Kacper Marcisz <kmarcisz@gmail.com>
2019-12-17 13:51:11 -0600

    Refactor the lambda app to make the code read easier

--- lambda/app.js
+++ lambda/app.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var dxf = require('dxf'),
-  request = require('request');
+const dxf = require('dxf');
+const request = require('request');
 
 exports.handler = function (event, context) {
   const body = JSON.parse(event.body);
 
   request({ url: body.url, encoding: null }, function (error, response, body) {
-    var dxfContents = body.toString();
-    var units = determineUnits(dxfContents);
-    var svgSTR = dxf.toSVG(dxf.parseString(dxfContents));
-    svgSTR = svgSTR.replace("INSUNITS", units);
-    svgSTR = svgSTR.replace("INSUNITS", units);
+    const dxfContents = body.toString();
+    const units = determineUnits(dxfContents);
+    const svgSTR = dxf
+      .toSVG(dxf.parseString(dxfContents))
+      .replace("INSUNITS", units);
 
     context.done(
       null,
@@ -26,27 +26,29 @@ exports.handler = function (event, context) {
 };
 
 function determineUnits(dxfContents) {
-  var unitSVG = "INSUNITS";
+  let unitSVG = "INSUNITS";
+
   if (dxfContents.includes("$INSUNITS")) {
-    var splitOnUnits = dxfContents.split("$INSUNITS");
-    var splitOnNine = splitOnUnits[1].split("9");
-    var splitOnSeventy = splitOnNine[0].split("70");
-    var units = splitOnSeventy[1].replace(/(\r\n\t|\n|\r\t)/gm, "");
-    var unit = parseInt(units);
+    const splitOnUnits = dxfContents.split("$INSUNITS");
+    const splitOnNine = splitOnUnits[1].split("9");
+    const splitOnSeventy = splitOnNine[0].split("70");
+    const units = splitOnSeventy[1].replace(/(\r\n\t|\n|\r\t)/gm, "");
+    const unit = parseInt(units);
 
     switch (unit) {
       case 1:
-        unitSVG = "in"
+        unitSVG = "in";
         break;
       case 4:
-        unitSVG = "mm"
+        unitSVG = "mm";
         break;
       case 5:
-        unitSVG = "cm"
+        unitSVG = "cm";
         break;
       default:
         unitSVG = "INSUNITS"
     }
   }
+
   return unitSVG;
 }
